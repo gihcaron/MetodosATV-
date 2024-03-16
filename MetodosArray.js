9cacb2ea2e1d2bf00f014d74c1d7ce1e11bf6677
Giovanna <giovannacaron.tds1.senai@gmail.com>
2024-03-16 15:05:53 -0300

    Metodo ForEach: Exemplo 1

---
+++ MetodosArray.js
@@ -0,0 +1,24 @@
+// Lista de Exercicios: Métodos Array
+
+// Metodo 1: ForEach ]
+
+//Sitaxe: array1.forEach((element) => console.log(element));
+
+//Exemplo 1 - ForEach
+
+// Declarar os números (Array) desejados
+
+const numbers = [4, 8, 28, 32];
+
+//Para cada núemro e index
+//Exibir "O Numero é X e está na posição index"
+
+numbers.forEach((numbers, index) => {
+    console.log(" O Numero é " + numbers + " e está na posição " + index);
+
+});
+
+
+//Exemplo 2 - ForEach: Como posso aplicar o ForEach na prática?
+
+// Metodo 2: 
